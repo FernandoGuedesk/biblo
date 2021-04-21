bb59dbe9fe26fb38c61dd11bf68adbab7cc92e7e
Fernando Guedes <nando3ryu@gmail.com>
2021-04-21 14:28:40 -0300

    Meu primeiro commit

---
+++ src/main/java/br/dev/nando/biblo/BibloApplication.java
@@ -0,0 +1,13 @@
+package br.dev.nando.biblo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/nando/biblo/BibloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.nando.biblo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
