913422a6d62b2a189dc9f5c7395e9188698bb16d
Jonathan Nascimento <nascimento.jonathan@live.com>
2021-07-20 20:26:02 -0300

    recurso listar todos os usuarios.

--- src/main/java/br/dev/nando/biblo/api/model/Usuario.java
+++ src/main/java/br/dev/nando/biblo/api/model/Usuario.java
@@ -1,11 +1,25 @@
 package br.dev.nando.biblo.api.model;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Usuario {
+	
+	@Id()
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idusuario")
 	private Long idUsuario;
+	
 	private String nome;
+	
 	private String email;
+	
 	private String tipo;
+	
 	private String telefone;
 	
 	
