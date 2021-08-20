14d6297f22cd9fcb1eb8c34c16997105734ccebf
Jonathan Nascimento <nascimento.jonathan@live.com>
2021-08-20 20:23:42 -0300

    correção de script.

---
+++ src/main/resources/db/migration/V007__alterar_coluna_status_emprestimo.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Table `biblo_bd`.`livros`
+-- -----------------------------------------------------
+
+ALTER TABLE livros CHANGE COLUMN statusEmprestimo status_emprestimo VARCHAR(25);
