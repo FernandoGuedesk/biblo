11962cab51b0ff9aeee2dc6b00487278eba79834
Fernando Guedes <nando3ryu@gmail.com>
2022-01-28 20:18:42 -0300

    Atualização e Cadastro de Histórico

--- src/main/java/br/dev/nando/biblo/api/controller/HistoricoController.java
+++ src/main/java/br/dev/nando/biblo/api/controller/HistoricoController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.dev.nando.biblo.api.model.Historico;
@@ -28,11 +32,16 @@ public class HistoricoController {
 		return repositorio.findAll();
 	}
 	
-	@ApiOperation(value = "Recupera um Historico específico")
+	@ApiOperation(value = "Buscar um Historico específico")
 	@GetMapping("/{idHistorico}")
 	public Optional<Historico> listarUmHistorico(@PathVariable Long idHistorico) {
-		Optional<Historico> historicoEncontrado = repositorio.findById(idHistorico);
-			return historicoEncontrado;
+		return  repositorio.findById(idHistorico);
+	}
+	@ApiOperation(value = "insere um historico")
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Historico adicionar(@RequestBody Historico historico) throws Exception {
+		return repositorio.save(historico);
 	}
 
 }
